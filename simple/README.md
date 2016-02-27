48c7ebf2b775bf663ba94f9db8ed21159e093688
Kelley Stover <kelley.stover@twintechs.com>
2016-02-26 16:16:34 -0800

    tidy

--- simple/README.md
+++ simple/README.md
@@ -1,13 +1,13 @@
 see http://spark.apache.org/docs/latest/quick-start.html
 
 ### Package a jar containing your application
-'''
+'''bash
 $ sbt package
 '''
 
 ### Use spark-submit to run your application
-'''
-$ YOUR_SPARK_HOME/bin/spark-submit \
+'''bash
+$ $SPARK_HOME/bin/spark-submit \
   --class "SimpleApp" \
   --master local[4] \
   target/scala-2.10/simple-project_2.10-1.0.jar
--- simple/src/main/scala/SimpleApp.scala
+++ simple/src/main/scala/SimpleApp.scala
@@ -7,12 +7,12 @@ object SimpleApp {
   def main(args: Array[String]) {
     val spk = sys.env.get("SPARK_HOME")
     val logFile = spk.get + "/README.md"
-    val conf = new SparkConf().setAppName("Simple Application")
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local");
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
-    println(logFile + ":  Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    println(">>>>>>>>>>>>>>>>>>>>   RESULT = " + logFile + ":  Lines with a: %s, Lines with b: %s".format(numAs, numBs))
   }
 }
 
